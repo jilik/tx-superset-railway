8412f3f4244ff986321fa90937b3be57dfffdbd5
Igor Zhelezniack <jstudio2008@gmail.com>
2025-09-25 17:39:10 +0300

    fix

--- config/superset_config.py
+++ config/superset_config.py
@@ -7,11 +7,4 @@ FEATURE_FLAGS = {
 PREVENT_UNSAFE_DB_CONNECTIONS = False
 ENABLE_PROXY_FIX = True
 
-# Берём секретный ключ из переменной окружения
-SECRET_KEY = os.environ.get("SECRET_KEY", "fallback_secret")
-
-# Подключение к мета-базе (PostgreSQL)
-SQLALCHEMY_DATABASE_URI = os.environ.get(
-    "DATABASE_URL",
-    "sqlite:////app/superset.db"  # fallback для локальной разработки
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
