2a0ea0a9f4c17ac9fc534b42f02370dd1b362945
Igor Zhelezniack <jstudio2008@gmail.com>
2025-09-25 17:54:53 +0300

    sdsd

--- config/superset_config.py
+++ config/superset_config.py
@@ -6,5 +6,10 @@ FEATURE_FLAGS = {
 
 PREVENT_UNSAFE_DB_CONNECTIONS = False
 ENABLE_PROXY_FIX = True
-
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# MySQL connection
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    "DATABASE_URL",
+    "mysql://superset:superset_password@mysql_host:3306/superset_db"
+)
--- config/superset_init.sh
+++ config/superset_init.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# create Admin user, you can read these values from env or anywhere else possible
-superset fab create-admin --username "$ADMIN_USERNAME" --firstname Superset --lastname Admin --email "$ADMIN_EMAIL" --password "$ADMIN_PASSWORD"
+# Создаем админа
+superset fab create-admin \
+    --username "$ADMIN_USERNAME" \
+    --firstname Superset \
+    --lastname Admin \
+    --email "$ADMIN_EMAIL" \
+    --password "$ADMIN_PASSWORD"
 
-# Upgrading Superset metastore
+# Обновляем базу
 superset db upgrade
 
-# setup roles and permissions
-superset superset init 
+# Инициализация ролей и прав
+superset init
 
-# Starting server
+# Запуск Superset
 /bin/sh -c /usr/bin/run-server.sh
